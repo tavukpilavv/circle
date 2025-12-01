8cb845222535b7ba8ecccb08aa8903978a1ef38a
busrahser <busra_kose24@icloud.com>
2025-12-02 02:00:39 +0300

    Fix: Increase password_hash column length to 256

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(db.Model):
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
     email = db.Column(db.String(120), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     
     # Signup Formu Alanları
     username = db.Column(db.String(64), unique=True)
