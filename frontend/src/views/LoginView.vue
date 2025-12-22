f18190ebd37c455f10ccb62260a5ab98222ca3c2
malakann9 <malojah.3li.96@gmail.com>
2025-12-23 02:53:11 +0300

    edits

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -34,7 +34,7 @@
                             <input type="checkbox" id="remember" checked>
                             <label for="remember">Remember me</label>
                         </div>
-                        <span @click="$router.push('/forgot-password')" class="forgot-password" style="cursor: pointer; position: relative; z-index: 10;">Forget password?</span>
+                        <a class="forgot-password" @click.prevent="$router.push({ name: 'ForgotPassword' })" style="cursor: pointer; position: relative; z-index: 10;">Forget password?</a>
                         
                     </div>
                 </form>
